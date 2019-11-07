ceeeb027e1b792f38ed84989221735b3cade9df0
rsknox <rsknox@users.noreply.github.com>
2019-11-07 17:51:59 -0600

    add lines to covert input to integer and manipulate

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,7 @@
 print ("Hello World")
-i = input("type integer")
-print("the number input was = ", i)
+i = input("type integer: ")
+i_int = int(i)
+print("the number input was = ", i_int)
+# add line to multiply and print input number
+j = i_int * 2
+print ("twice the input = ", j)
