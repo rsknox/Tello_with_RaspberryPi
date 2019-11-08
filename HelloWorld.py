358c3b58d52b33825f3012b5b66a7700302ea3b9
rsknox <rsknox@users.noreply.github.com>
2019-11-07 18:11:11 -0600

    another calc and print

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,3 +5,6 @@ print("the number input was = ", i_int)
 # add line to multiply and print input number
 j = i_int * 2
 print ("twice the input = ", j)
+# another calc and print statement
+k = j + j
+print ("input value: ", i, "   four times the input value: ", k)
